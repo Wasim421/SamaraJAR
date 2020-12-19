woesss/JL-Mod

Fix NPE in MidletThread

app/src/main/java/javax/microedition/shell/MidletThread.java
@@ -70,7 +70,8 @@ public class MidletThread extends HandlerThread implements Handler.Callback {
 	}
 
 	static void pauseApp() {
-		instance.mHandler.obtainMessage(PAUSE).sendToTarget();
+		if (instance != null)
+			instance.mHandler.obtainMessage(PAUSE).sendToTarget();
 	}
 
 	static void resumeApp() {
@@ -94,7 +95,8 @@ public class MidletThread extends HandlerThread implements Handler.Callback {
 			Display.postEvent(CanvasEvent.getInstance(canvas, CanvasEvent.KEY_PRESSED, keyCode));
 			Display.postEvent(CanvasEvent.getInstance(canvas, CanvasEvent.KEY_RELEASED, keyCode));
 		}
-		instance.mHandler.obtainMessage(DESTROY, 1).sendToTarget();
+		if (instance != null)
+			instance.mHandler.obtainMessage(DESTROY, 1).sendToTarget();
 	}
 
 	@Override

Wasim421/SamaraJAR

app/src/main/java/javax/microedition/shell/MidletThread.java
@@ -95,7 +95,8 @@ public class MidletThread extends HandlerThread implements Handler.Callback {
 			Display.postEvent(CanvasEvent.getInstance(canvas, CanvasEvent.KEY_PRESSED, keyCode));
 			Display.postEvent(CanvasEvent.getInstance(canvas, CanvasEvent.KEY_RELEASED, keyCode));
 		}
-		instance.handler.obtainMessage(DESTROY, 1).sendToTarget();
+		if (instance != null)
+			instance.handler.obtainMessage(DESTROY, 1).sendToTarget();
 	}
 
 	@Override